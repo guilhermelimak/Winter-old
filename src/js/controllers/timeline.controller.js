d656a3cd2bd0bbb05b600f2dfca8f6b092224081
g4 <guilhermelimak@gmail.com>
2015-12-31 06:25:31 -0200

    LIXO

--- src/js/controllers/timeline.controller.js
+++ src/js/controllers/timeline.controller.js
@@ -1,8 +1,6 @@
 angular
 .module('winter')
 .controller('TimelineController', ['$scope','Twitter', function($scope, Twitter) {
-
-
 	$scope.getToken = function() 	{
 		return JSON.parse(window.localStorage.getItem('accessTokenObject'));
 	};
@@ -10,21 +8,46 @@ angular
 	$scope.getTweets = function() {
 		var token = $scope.getToken();
 		var client = new Twitter;
-
+		//do a get Timeline first to populate the timeline and after that getstream to keep geting statuses in realtime
 		client.getTimeline('home',
-			null,
+			null,	
 			token.accessToken,
 			token.accessTokenSecret,
 			(error, data, response) => {
 				if (error) {
 					console.log("fila da puta");
+					console.log(error);
 				} else {
-					console.log(data);
+					console.log(error)
+					console.log(response);
+					console.log(data + "lol");
 					$scope.tweets = data;
 					$scope.$apply();
+				}
+			})
+
+		client.getStream('user',
+			{"with": "followingds"},
+			token.accessToken,
+			token.accessTokenSecret,
+			(error, data, response) => {
+				if (error) {
+					console.log(error);
+				} else { 
+					console.log(data + "rox");
+					console.log(data);
+					$scope.$apply();
+				}
+			},			
+			(error, data, response) => {
+				if (error) {
+					console.log("eita porra");
+				} else {
+					console.log(data);
 				}
 			}
 		);
 	}
+
 	$scope.getTweets();
-}]);
+}])
