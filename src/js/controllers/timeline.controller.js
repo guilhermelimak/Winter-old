b5d6c17374c6649140a8c9c82099aa7b7777c177
Armando Magalhaes <armando.mag95@gmail.com>
2016-01-01 13:35:47 -0200

    for god sakess

--- src/js/controllers/timeline.controller.js
+++ src/js/controllers/timeline.controller.js
@@ -1,88 +1,65 @@
 angular
 .module('winter')
 .controller('TimelineController', ['$scope', '$interval', 'Twitter', 'hotkeys', function($scope, $interval, Twitter, hotkeys) {
-	$scope.initialize = function() {
-		$scope.client = new Twitter;
+	const client = new Twitter;
 
-		$scope.getToken();
+	$scope.initialize = initialize;
+	$scope.getTweets = getTweets;
+	$scope.startStream = startStream;
+	$scope.retweet = retweet;
+	$scope.favorite = favorite;
+	$scope.reply = reply;
+
+	function initialize() {
 		$scope.tweets = [];
+
 		hotkeys.add({
 	    combo: 'n',
 	    description: 'new tweet',
 	    callback: function() {
 	    	console.log('irra');
-    }
-  });
-	}
-	
-	$scope.getToken = function() 	{
-		$scope.token = JSON.parse(window.localStorage.getItem('accessTokenObject'));
-	};
+	    }
+	  });
 
-	$scope.getTweets = function() {
-		//do a get Timeline first to populate the timeline and after that getstream to keep geting statuses in realtime
-		$scope.client.getTimeline('home',
-			null,
-			$scope.token.accessToken,
-			$scope.token.accessTokenSecret,
-			(error, data, response) => {
-				if (error) {
-					console.log(error);
-				} else {
-					$scope.tweets = data;
-					$scope.$apply();
-				}
-			})
-	};
-
-	$scope.startStream = function() {
-		$scope.client.getStream('user',
-			{ "with": "followingds" },
-			$scope.token.accessToken,
-			$scope.token.accessTokenSecret,
-			(error, data, response) => {
-				if (error) {
-					console.error(error);
-				} else {
-					console.log(data);
-
-					if (Object.keys(data).length != 0 &&
-							data.friends == undefined &&
-							data.created_at !== undefined) {
-						$scope.tweets.unshift(data);
-						$scope.$apply();
-					}
-				}
-			},
-			(error, data, response) => { return }
-		);
+		$scope.getTweets();
+		$scope.startStream();
 	}
 
-	$scope.retweet = function(tweet) {
-		$scope.client.statuses('retweet', 
-			tweet,
-			$scope.token.accessToken,
-			$scope.token.accessTokenSecret,
-			(error, data, response) => {
-				if (error) {
-					console.log(error);
-					console.log(tweet);
-				} else {
-					console.log(response);
-				}
-			});
-	};
+	function getTweets() {
+		client.getTimeline('home', (data, response) => {
+			$scope.tweets = data;
+			$scope.$apply();
+		});
+	}
 
-	$scope.favorite = function(tweet) {
+	function startStream() {
+		client.getStream('user', { "with": "followingds" }, (data, response) => {
+			console.log(data);
+
+			if (Object.keys(data).length != 0 &&
+					data.friends == undefined &&
+					data.created_at !== undefined) {
+				$scope.tweets.unshift(data);
+				$scope.$apply();
+			}
+		});
+	}
+
+	function retweet(tweet) {
+		console.log(tweet);
+		// client.statuses('retweet', tweet, (data, response) => {
+		// 	console.log(data);
+		// 	console.log(response);
+		// });
+	}
+
+	function favorite(tweet) {
 		console.log("favorite");
-	};
+	}
 
-	$scope.reply = function(tweet) {
+	function reply(tweet) {
 		console.log("reply");
-	};
+	}
 
 	$scope.initialize();
-
-	$scope.getTweets();
-	$scope.startStream();
 }]);
--- src/js/services/twitter.service.js
+++ src/js/services/twitter.service.js
@@ -10,18 +10,66 @@
   .module('winter')
   .factory('Twitter', () => {
     return function(accessToken) {
+      var twitter = null;
+
       if (!accessToken) {
-        return new Twitter(Credentials);
+        twitter = new Twitter(Credentials);
+      } else {
+        var authorizedCredentials = {
+          "consumer_key":        Credentials['consumerKey'],
+          "consumer_secret":     Credentials['consumerSecret'],
+          "access_token_key":    accessToken['accessToken'],
+          "access_token_secret": accessToken['accessTokenSecret'],
+        };
+
+        twitter = new Twitter(authorizedCredentials);
       }
 
-      var authorizedCredentials = {
-        "consumer_key":        Credentials['consumerKey'],
-        "consumer_secret":     Credentials['consumerSecret'],
-        "access_token_key":    accessToken['accessToken'],
-        "access_token_secret": accessToken['accessTokenSecret'],
-      };
+      return {
+        getTimeline(type, callback, params) {
+          twitter.getTimeline(type, params || null,
+      			authorizedCredentials["access_token_key"],
+      			authorizedCredentials["access_token_secret"],
+      			(error, data, response) => {
+      				if (error) {
+      					console.error(error);
+                return;
+      				}
 
-      return new Twitter(authorizedCredentials);
+              return callback(data, response);
+      			}
+          );
+        },
+        getStream(type, params, dataCallback, endCallback) {
+          twitter.getStream(type, params || null,
+            authorizedCredentials["access_token_key"],
+      			authorizedCredentials["access_token_secret"],
+      			(error, data, response) => {
+      				if (error) {
+      					console.error(error);
+                return;
+      				}
+
+              return dataCallback(data, response);
+      			},
+      			endCallback || angular.noop
+      		);
+        },
+        statuses(type, params, callback) {
+          $scope.client.statuses(type, params,
+            authorizedCredentials["access_token_key"],
+      			authorizedCredentials["access_token_secret"],
+      			(error, data, response) => {
+      				if (error) {
+      					console.error(error);
+      					return;
+      				}
+
+              return callback(data, response);
+      			}
+          );
+        }
+      };
     }
   });
 })();
