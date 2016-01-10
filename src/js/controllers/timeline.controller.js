0df48503a95a33db15388c317e43b285e0fb6131
Armando Magalhaes <armando.mag95@gmail.com>
2016-01-09 22:51:09 -0200

    DESRETUITANDO ISSO MESMO

--- src/js/controllers/timeline.controller.js
+++ src/js/controllers/timeline.controller.js
@@ -3,13 +3,18 @@
 
 	angular
 	.module('winter')
-	.controller('TimelineController', ['$scope', '$interval', 'Twitter','$sce', 'Modal', 'HotkeyRegistry', ($scope, $interval, Twitter, $sce, Modal, HotkeyRegistry) => {
+	.controller('TimelineController', ['$scope', '$interval', 'Twitter','$sce', 'Modal', 'HotkeyRegistry', 'UserStorage', ($scope, $interval, Twitter, $sce, Modal, HotkeyRegistry, UserStorage) => {
+		const logging = false;
 		const client = new Twitter();
 
 		$scope.tweets = [];
+		$scope.retweets = [];
+
+		$scope.user = UserStorage.user;
 
 		$scope.logTweet = (tweet) => {
-			console.log(tweet);
+			if (logging)
+				console.log(tweet);
 		};
 
 		$scope.getTweets = getTweets;
@@ -23,9 +28,81 @@
 		function _initialize() {
 			new HotkeyRegistry();
 
-			$scope.tweets = [];
-			$scope.getTweets();
-			$scope.startStream();
+			getTweets();
+			startStream();
+		}
+
+		function getTweets() {
+			client.getTimeline('home', (data, response) => {
+				$scope.tweets = data.map(_detectLinks);
+				$scope.$apply();
+			});
+		}
+
+		function startStream() {
+			client.getStream('user', { "with": "followings" }, (data, response) => {
+				var wasRetweetedByUser = _wasRetweetedByUser(data)
+				,		hasKeys = Object.keys(data).length != 0;
+
+				if (_receivedReply(data)) {
+					_throwNotification(data);
+				}
+
+				if (wasRetweetedByUser) {
+					data.retweeted_by_user = true;
+				}
+
+				if (data.friends == undefined &&
+						data.created_at !== undefined &&
+						hasKeys &&
+						!data.retweeted_by_user) {
+					$scope.tweets.unshift(_detectLinks(data));
+					$scope.$apply();
+				}
+			});
+		}
+
+		function retweet(tweet, index) {
+			if (tweet.retweeted || tweet.retweeted_by_user) {
+				client.statuses("destroy", { id: tweet.retweet_id_str }, () => {
+					$scope.tweets[index].retweet_id_str = null;
+					$scope.tweets[index].retweeted_by_user = false;
+					$scope.tweets[index].retweet_count--;
+					$scope.$apply();
+				});
+			} else {
+				client.statuses("retweet", { id: tweet.id_str }, (data, response) => {
+					$scope.tweets[index].retweet_id_str = data.id_str;
+					$scope.tweets[index].retweeted_by_user = true;
+					$scope.tweets[index].retweet_count++;
+					$scope.$apply();
+				});
+			}
+		}
+
+		function favorite(tweet) {
+			var type = tweet.favorited ? 'destroy' : 'create';
+
+			client.favorites(type, { id: tweet.id_str }, () => {
+				tweet.favorited = !tweet.favorited;
+				$scope.$apply();
+			});
+		}
+
+		function reply(replyObject) {
+			client.statuses('update', replyObject, angular.noop);
+		}
+
+		function _wasRetweetedByUser(data) {
+			return data.retweeted || (data.retweeted_status && data.user.screen_name == UserStorage.user.screen_name);
+		}
+
+		function _receivedReply(data) {
+			return (data.in_reply_to_screen_name === UserStorage.user.screen_name);
+		}
+
+		function _throwNotification(data) {
+			new Notification(`You got a mention from @${data.user.screen_name}: ${data.text}`);
 		}
 
 	  function _detectLinks(tweet) {
@@ -42,51 +119,6 @@
 
 	    return tweet;
 	  }
-
-		function getTweets() {
-			client.getTimeline('home', (data, response) => {
-				for (var i = data.length; i--;) {
-					data[i] = _detectLinks(data[i])
-				}
-
-				$scope.tweets = data;
-				$scope.$apply();
-			});
-		}
-
-		function startStream() {
-			var userData = JSON.parse(window.localStorage.getItem('userData'));
-			console.log(userData);
-
-			client.getStream('user', { "with": "followings" }, (data, response) => {
-				if (Object.keys(data).length != 0 &&
-						data.friends == undefined &&
-						data.created_at !== undefined) {
-
-					if (data.in_reply_to_screen_name === userData.screen_name) {
-						new Notification(`You got a mention from @${data.user.screen_name}: ${data.text}`)
-					}
-
-					$scope.tweets.unshift(_detectLinks(data));
-					$scope.$apply();
-				}
-			});
-		}
-
-		function retweet(tweet) {
-			client.statuses("retweet", { id: tweet.id_str }, angular.noop);
-		}
-
-		function favorite(tweet) {
-			var type = tweet.favorited ? 'destroy' : 'create';
-			tweet.favorited = !tweet.favorited;
-
-			client.favorites(type, { id: tweet.id_str }, angular.noop);
-		}
-
-		function reply(replyObject) {
-			client.statuses('update', replyObject, angular.noop);
-		}
 
 		_initialize();
 	}]);
