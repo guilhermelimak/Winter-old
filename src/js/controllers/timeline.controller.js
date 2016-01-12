d471c55d0091da1f3f3b83f678d60d29477f206d
Armando Magalhaes <armando.mag95@gmail.com>
2016-01-12 01:22:06 -0200

    timeline

--- src/js/controllers/timeline.controller.js
+++ src/js/controllers/timeline.controller.js
@@ -4,18 +4,13 @@
 	angular
 	.module('winter')
 	.controller('TimelineController', ['$scope', '$interval', 'Twitter','$sce', 'Modal', 'HotkeyRegistry', 'UserStorage', ($scope, $interval, Twitter, $sce, Modal, HotkeyRegistry, UserStorage) => {
-		const logging = false;
 		const client = new Twitter();
 
 		$scope.tweets = [];
-		$scope.retweets = [];
 
 		$scope.user = UserStorage.user;
 
-		$scope.logTweet = (tweet) => {
-			if (logging)
-				console.log(tweet);
-		};
+		$scope.logTweet = (tweet) => console.log(tweet);
 
 		$scope.getTweets = getTweets;
 		$scope.startStream = startStream;
@@ -50,33 +45,30 @@
 
 				if (wasRetweetedByUser) {
 					data.retweeted_by_user = true;
+
+					client.statuses("show", { id: data.id_str }, (data, response) => {
+						console.log(data, response);
+					});
 				}
 
 				if (data.friends == undefined &&
 						data.created_at !== undefined &&
 						hasKeys &&
 						!data.retweeted_by_user) {
+					console.log(data, 'added');
 					$scope.tweets.unshift(_detectLinks(data));
 					$scope.$apply();
+				} else {
+					console.log(data, wasRetweetedByUser ? 'retweet not added' : 'not added');
 				}
 			});
 		}
 
 		function retweet(tweet, index) {
 			if (tweet.retweeted || tweet.retweeted_by_user) {
-				client.statuses("destroy", { id: tweet.retweet_id_str }, () => {
-					$scope.tweets[index].retweet_id_str = null;
-					$scope.tweets[index].retweeted_by_user = false;
-					$scope.tweets[index].retweet_count--;
-					$scope.$apply();
-				});
+				client.statuses("destroy", { id: tweet.retweet_id_str }, () => _toggleRetweet(index));
 			} else {
-				client.statuses("retweet", { id: tweet.id_str }, (data, response) => {
-					$scope.tweets[index].retweet_id_str = data.id_str;
-					$scope.tweets[index].retweeted_by_user = true;
-					$scope.tweets[index].retweet_count++;
-					$scope.$apply();
-				});
+				client.statuses("retweet", { id: tweet.id_str }, (data, response) => _toggleRetweet(index, data));
 			}
 		}
 
@@ -93,6 +85,10 @@
 			client.statuses('update', replyObject, angular.noop);
 		}
 
+		function _getRetweetStatus(tweet) {
+			client.statuses('retweet', { id: tweet.id_str })
+		}
+
 		function _wasRetweetedByUser(data) {
 			return data.retweeted || (data.retweeted_status && data.user.screen_name == UserStorage.user.screen_name);
 		}
@@ -103,6 +99,19 @@
 
 		function _throwNotification(data) {
 			new Notification(`You got a mention from @${data.user.screen_name}: ${data.text}`);
+		}
+
+		function _toggleRetweet(index, data) {
+			if (!data) {
+				$scope.tweets[index].retweet_count--;
+				$scope.tweets[index].retweet_id_str = null;
+			} else {
+				$scope.tweets[index].retweet_count++;
+				$scope.tweets[index].retweet_id_str = data.id_str;
+			}
+
+			$scope.tweets[index].retweeted_by_user = !$scope.tweets[index].retweeted_by_user;
+			$scope.$apply();
 		}
 
 	  function _detectLinks(tweet) {
