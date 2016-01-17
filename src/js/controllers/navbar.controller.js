688ab53e53a9524e5c25ee0c5e33b7cbd5a8b2ba
g4 <guilhermelimak@gmail.com>
2016-01-17 02:51:27 -0200

    new tweet button

--- src/js/controllers/navbar.controller.js
+++ src/js/controllers/navbar.controller.js
@@ -3,7 +3,9 @@
 
 	angular
 	.module('winter')
-	.controller('navbarController', ['$scope', '$location', ($scope, $location) => {
+	.controller('navbarController', ['$scope', '$location', 'Modal', ($scope, $location, Modal) => {
+		$scope.showNewTweetModal = Modal.showNewTweetModal;
+
 		$scope.goHome = function() {
 			$location.url('/timeline')
 		};
